00a1b0bf6611e8d3c2728047e7a8a30164d78235
Craine Runton <craine.runton@distilnetworks.com>
2016-08-26 08:20:03 -0600

    Update monitored_hosts.js example for new build

--- node_app/example_monitored_hosts.js
+++ node_app/example_monitored_hosts.js
@@ -1,12 +1,31 @@
 var hosts = [
-  'www.google.com',
-  'www.twitter.com',
-  'sha256.badssl.com',
-  'expired.badssl.com',
-  'self-signed.badssl.com',
-  'wrong.host.badssl.com',
-  'incomplete-chain.badssl.com',
-  'example.dev',
-  'vsaq.dev'
+  {
+    hostname: 'www.google.com'
+  },
+  {
+    hostname: 'www.runtondev.com'
+  },
+  {
+    hostname: 'sha256.badssl.com'
+  },
+  {
+    hostname: 'expired.badssl.com'
+  },
+  {
+    hostname: 'self-signed.badssl.com'
+  },
+  {
+    hostname: 'wrong.host.badssl.com'
+  },
+  {
+    hostname: 'incomplete-chain.badssl.com'
+  },
+  {
+    hostname: 'example.dev',
+    port: 8080
+  },
+  {
+    hostname: 'vsaq.dev'
+  }
 ];
 module.exports = hosts;
